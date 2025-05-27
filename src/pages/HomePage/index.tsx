e1edd510394a0ab83398c8685899d1dbb782dc3b
Sada Shukurova <123108040+Sada-Shukurova@users.noreply.github.com>
2025-05-28 01:34:28 +0400

    add home page

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,9 @@
+const HomePage: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-6xl">Coming soon...</h1>
+    </div>
+  );
+};
+
+export default HomePage;
