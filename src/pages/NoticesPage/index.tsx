09d6bfc3ad2c39b5123b3af30bf62d99d02d1c2f
Sada Shukurova <123108040+Sada-Shukurova@users.noreply.github.com>
2025-05-28 01:34:40 +0400

    add notices page

---
+++ src/pages/NoticesPage/index.tsx
@@ -0,0 +1,11 @@
+import MyTable from "../../components/MyTable";
+
+const NoticesPage: React.FC = () => {
+  return (
+    <div className="container mx-auto">
+      <MyTable />
+    </div>
+  );
+};
+
+export default NoticesPage;
