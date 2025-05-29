9dab160553233854da1259a6eabde8327b661e1e
Sada Shukurova <123108040+Sada-Shukurova@users.noreply.github.com>
2025-05-29 12:18:04 +0400

    update header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,32 @@
 import { NavLink } from "react-router-dom";
 import ThemeToggler from "../ThemeToggler";
+import { motion } from "framer-motion";
 
 const Header: React.FC = () => {
   return (
     <>
       <div className="navbar bg-accent-content shadow-md rounded-bl-box rounded-br-box sticky top-0 z-10">
         <div className="container justify-between items-center flex mx-auto">
-          <div>
+          <motion.div
+            initial={{ x: -250 }}
+            animate={{ x: -10 }}
+            transition={{ delay: 0.2, type: "spring", stiffness: 120 }}
+          >
             <NavLink
               to={"/"}
               className="btn btn-ghost text-xl font-black transition-all duration-500"
             >
               ||NicheSolutions
             </NavLink>
-          </div>
-          <div className="flex  items-center gap-10">
-            <NavLink to={"/notices?page=1"}>Notices</NavLink>
+          </motion.div>
+          <motion.div
+            initial={{ x: 250 }}
+            animate={{ x: 10 }}
+            transition={{ delay: 0.2, type: "spring", stiffness: 120 }}
+            className="flex  items-center gap-10"
+          >
             <ThemeToggler />
-          </div>
+          </motion.div>
         </div>
       </div>
     </>
