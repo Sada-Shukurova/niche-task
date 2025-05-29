4582df18b5f26868fa28ba1f97e8db82b2f061bb
Sada Shukurova <123108040+Sada-Shukurova@users.noreply.github.com>
2025-05-29 12:18:18 +0400

    update home page, add animation

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,8 +1,60 @@
+import { motion } from "motion/react";
+import { NavLink } from "react-router-dom";
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3,
+      delayChildren: 0.5,
+    },
+  },
+  exit: {
+    x: "-100vw",
+    opacity: 0,
+    transition: {
+      ease: "easeInOut",
+    },
+  },
+};
+const childVariants = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      ease: "easeOut",
+    },
+  },
+};
 const HomePage: React.FC = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h1 className="text-6xl">Coming soon...</h1>
-    </div>
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      className="flex gap-10 flex-col justify-center items-center min-h-screen"
+    >
+      <div className="text-6xl text-center leading-relaxed">
+        <motion.p variants={childVariants}>Your Tech Partner</motion.p>
+        <motion.p variants={childVariants}>in Real Estate</motion.p>
+        <motion.p variants={childVariants}>Innovation</motion.p>
+      </div>
+      <motion.button
+        variants={childVariants}
+        className="btn btn-primary hover:scale-110 transition-all duration-300"
+      >
+        <NavLink to={"/notices?page=1"}>Explore Notices</NavLink>
+      </motion.button>
+    </motion.div>
   );
 };
 
